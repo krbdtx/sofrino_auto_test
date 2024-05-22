b030e4b190ee136b6d607b2179de8cda35b999eb
Женя <dtxtrump@gmail.com>
2024-05-23 00:24:21 +0300

    fix 2

--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -5,7 +5,7 @@ from faker import Faker
 fake = Faker()
 
 user1 = User_login(email='фыв@фыв.com', password='123123')
-user2 = User_login(email='111@222.com', password='123123123')
+user2 = User_login(email='11asasdasd1@2asdasasd22.com', password='фывфвфыв123123123asdasdasd')
 user3 = User_login(email=fake.email(), password=fake.password())
 
 
