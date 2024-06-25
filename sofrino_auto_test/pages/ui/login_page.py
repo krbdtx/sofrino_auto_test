882eddafbc74b7f2753e00e0b4226f07e87e0e26
krbdtx <dtxtrump@gmail.com>
2024-06-25 12:11:39 +0300

    refactor to name

---
+++ sofrino_auto_test/pages/ui/login_page.py
@@ -0,0 +1,67 @@
+import allure
+
+from sofrino_auto_test.test_data import Userlogin
+from selene import browser, have, by
+
+
+class LogIn:
+
+    @allure.step('Открыть браузер')
+    def open_browser(self, url) -> object:
+        browser.open(url)
+        return self
+
+    @allure.step('Заполнить поле E-mail:')
+    def fill_login_mail_low(self, e_mail):
+        browser.element('[type="text"]').type(e_mail)
+        return self
+
+    @allure.step('Заполнить поле Пароль:')
+    def fill_login_password_low(self, password):
+        browser.element('[type="password"]').type(password)
+        return self
+
+    @allure.step("Нажать кнопку войти")
+    def submit_login_low(self):
+        browser.element(by.text('Войти')).click()
+        return self
+
+    @allure.step("Нажать кнопку Выход")
+    def submit_logout_low(self):
+        browser.element(by.text('Выход')).click()
+        return self
+
+    @allure.step("Проверка не успешного входа Неправильный E-mail / пароль")
+    def should_error_page_login_low(self):
+        browser.element('.text-danger').should(have.exact_text('Неправильный E-mail / пароль'))
+        return self
+
+    @allure.step("Проверка успешного входа")
+    def should_succes_page_login_low(self):
+        browser.element('.shop-menu__link').should(have.exact_text('Корзина товаров'))
+        return self
+
+
+class StepsLogIn:
+
+    @allure.step('Заполнить поля для авториазции')
+    def fill_login_page(self, value: Userlogin):
+        login.open_browser(url='/login')
+        login.fill_login_mail_low(value.email)
+        login.fill_login_password_low(value.password)
+        login.submit_login_low()
+        return self
+
+    @allure.step('Проверка Вход с некоректными данными')
+    def should_error_login_page(self):
+        login.should_error_page_login_low()
+        return self
+
+    @allure.step('Проверка Вход с коректными данными')
+    def should_login_user_lk_page(self):
+        login.should_succes_page_login_low()
+        return self
+
+
+stepslogin = StepsLogIn()
+login = LogIn()
---
+++ sofrino_auto_test/pages/ui/register_page.py
@@ -0,0 +1,96 @@
+import allure
+
+from sofrino_auto_test.test_data import Userdata
+from selene import browser, have, be
+
+
+class Registration:
+
+    @allure.step('Открыть браузер')
+    def open_browser(self, url) -> object:
+        browser.open(url)
+        return self
+
+    @allure.step("Ввод Фамилии")
+    def fill_user_last_name(self, last_name):
+        browser.element('#register').all('.form-group').element_by(have.text('Фамилия:')).element('.form-control').type(
+            last_name)
+        return self
+
+    @allure.step("Ввод Имени")
+    def fill_user_first_name(self, first_name):
+        browser.element('#register').all('.form-group').element_by(have.text('Имя:')).element('.form-control').type(
+            first_name)
+        return self
+
+    @allure.step("Ввод Телефона")
+    def fill_user_phone(self, phone):
+        browser.element('#register').all('.form-group').element_by(have.text('Телефон:')).element('.form-control').type(
+            phone)
+        return self
+
+    @allure.step("Ввод Почты")
+    def fill_user_email(self, email):
+        browser.element('#register').all('.form-group').element_by(have.text('E-mail:')).element('.form-control').type(
+            email)
+        return self
+
+    @allure.step("Ввод Пароля")
+    def fill_user_password(self, password):
+        browser.element('#register').all('.form-group').element_by(have.text('Пароль:')).element('.form-control').type(
+            password)
+        return self
+
+    @allure.step("Нажать кнопку Зарегистрироваться")
+    def submit_register_user(self):
+        browser.element('#register').element('.row').element('.btn').click()
+        return self
+
+    @allure.step("Проверка успешная регистрация")
+    def should_succes_register_user(self):
+        browser.element('[role="alert"]').should(have.text(
+            'Вы успешно зарегистрированы. Мы выслали вам данные для входа на e-mail.'))
+        return self
+
+    @allure.step("Проверка поля адрес почты НЕ верный адрес почты")
+    def should_error_mail_register_user(self):
+        browser.element('#register').element('.border-danger').should(be.visible)
+        return self
+
+    @allure.step("Проверка поля пароль НЕ верный пароль")
+    def should_error_pass_register_user(self):
+        browser.element('#register').element('.border-danger').should(be.visible)
+        return self
+
+
+class StepsRegistration:
+
+    @allure.step('Заполнить поля регистрации')
+    def fill_register_user(self, value: Userdata):
+        registration.open_browser(url='/login?state=register')
+        registration.fill_user_last_name(value.first_name)
+        registration.fill_user_first_name(value.first_name)
+        registration.fill_user_phone(value.phone_number)
+        registration.fill_user_email(value.email)
+        registration.fill_user_password(value.password)
+        registration.submit_register_user()
+        return self
+
+    @allure.step('Проверить успешность регистрации')
+    def should_register_user(self):
+        registration.should_succes_register_user()
+        return self
+
+    @allure.step('Проверить НЕ верный адрес почты')
+    def should_error_mail_user(self):
+        registration.should_error_mail_register_user()
+        return self
+
+    @allure.step('Проверить пустой пароль')
+    def should_error_pass_user(self):
+        registration.should_error_pass_register_user()
+        return self
+
+
+stepsregistration = StepsRegistration()
+registration = Registration()
